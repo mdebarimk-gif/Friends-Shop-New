34b22c583e91a541b5feec47b0b0d3eaf142383e
md ebrahim <mdebarimk@gmail.com>
2026-09-03 23:21:35 +0600

    Link customer account to account page

--- app/layout.tsx
+++ app/layout.tsx
@@ -105,7 +105,7 @@ export default function RootLayout({
             </a>
 
             <a
-              href="/admin"
+              href="/account"
               style={{
                 textDecoration: "none",
                 color: "#555",
